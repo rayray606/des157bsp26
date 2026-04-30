42d542d69677d1d4c93cc4a75fdbe4f9f5e0aef6
rayray606 <rsbyang@ucdavis.edu>
2026-04-29 23:20:49 -0700

    leaflet

---
+++ leaflet/script.js
@@ -0,0 +1,48 @@
+(function(){
+    'use strict';
+
+    // add your script here
+    var map = L.map('map').setView([51.505, -0.09], 13);
+    var marker = L.marker([38.249359, -122.039963]).addTo(map);
+    var circle = L.circle([38.249359, -122.039963], {
+    color: 'red',
+    fillColor: '#f03',
+    fillOpacity: 0.5,
+    radius: 500
+}).addTo(map);
+var polygon = L.polygon([
+    [51.509, -0.08],
+    [51.503, -0.06],
+    [51.51, -0.047]
+]).addTo(map);
+var popup = L.popup()
+    .setLatLng([38.249359, -122.039963])
+    .setContent("I am a standalone popup.")
+    .openOn(map);
+
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
+    
+    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+circle.bindPopup("I am a circle.");
+polygon.bindPopup("I am a polygon.");
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(map);
+}
+
+map.on('click', onMapClick);
+
+
+}());
