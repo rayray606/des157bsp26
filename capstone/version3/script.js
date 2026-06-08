119d895f289992a381ee535d9ace9e20859e9d5c
rayray606 <rsbyang@ucdavis.edu>
2026-06-08 15:45:16 -0700

    update

--- capstone/version3/script.js
+++ capstone/version3/script.js
@@ -1,5 +1,12 @@
 // AOS
 
+const overlay = document.querySelector("#overlay");
+const closePopup = document.querySelector("#closePopup");
+
+closePopup.addEventListener("click", () => {
+    overlay.style.display = "none";
+    });
+    
 AOS.init();
 
 
@@ -48,13 +55,14 @@ noChangeBtn.addEventListener("click", () => {
 
     futureText.innerHTML = `
 
-    <h3>Future Without Change</h3>
+  <h3>Future Without Change</h3>
 
     <p>
-    If mental health remains hidden,
-    many pastor's kids may experience
-    burnout, isolation, and pressure
-    to appear perfect.
+        In this future, mental health continues to be a difficult topic within many Korean church communities. Pastor's kids still feel pressure to appear strong, obedient, and spiritually mature, even when they are struggling internally. Because of stigma and fear of judgment, many choose not to seek help or talk openly about anxiety, depression, burnout, or identity struggles.
+    </p>
+
+    <p>
+        As these pressures continue, some young people may become disconnected from their faith communities or feel isolated from their families. Churches focus  on maintaining traditions and appearances rather than addressing emotional well-being. The cycle of silence continues into future generations, making it difficult for meaningful change to occur.
     </p>
 
     `;
@@ -66,11 +74,12 @@ supportBtn.addEventListener("click", () => {
 
     <h3>Future With Support</h3>
 
+<p>
+        In this future, churches recognize mental health as an important part of overall well-being. Church leaders openly discuss topics such as stress, anxiety, burnout, and counseling. Pastor's kids feel more comfortable sharing their experiences without fear of shame or judgment.
+    </p>
+
     <p>
-    Churches could create healthier
-    environments through counseling,
-    support groups, and open discussions
-    about mental health.
+        Support groups, mentorship programs, and mental health resources become common within church communities. Families and congregations learn how to balance faith with emotional health creating environments where young people feel seen, heard, and supported. Future generations grow up with healthier relationships, stronger identities, and a deeper sense of belonging within their communities.
     </p>
 
     `;
@@ -124,49 +133,79 @@ submitReflection.addEventListener("click", () => {
 
 // CHART
 
-const ctx =
-document.getElementById("myChart");
+const ctx1 = document.getElementById("myChart1");
 
-new Chart(ctx, {
-
+new Chart(ctx1, {
     type: "bar",
-
     data: {
-
         labels: [
-
-            "Pressure",
-            "Stigma",
-            "Isolation",
-            "Support"
-
+            "Asian Americans with Mental Health Issues",
+            "Asian Americans Seeking Help",
+            "Older Korean Americans with Depression",
+            "Older Korean Americans Accessing Help"
         ],
-
         datasets: [{
-
-            label:
-            "Mental Health Factors",
-
-            data: [
-
-                20,
-                33,
-                19,
-                22
-
+            label: "Percent (%)",
+            data: [10, 10, 34, 6.5],
+            backgroundColor: [
+                "#9e88f4",
+                "#b5a2f8",
+                "#7c68db",
+                "#d6cbff"
             ]
-
         }]
     },
-
     options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: "Mental Health Needs vs Professional Help-Seeking"
+            }
+        },
+        scales: {
+            y: {
+                beginAtZero: true,
+                max: 40
+            }
+        }
+    }
+});
 
-        responsive:true,
+const ctx2 = document.getElementById("myChart2");
 
-        scales:{
-            y:{
-                beginAtZero:true,
-                max:50
+new Chart(ctx2, {
+    type: "pie",
+    data: {
+        labels: [
+            "Do Not Need Therapy",
+            "Don't Know Any Counselors",
+            "Too Expensive",
+            "Unsure How Counseling Helps",
+            "Too Busy",
+            "Saving Face"
+        ],
+        datasets: [{
+            data: [46, 21, 19, 10, 2, 2],
+            backgroundColor: [
+                "#88b7f4",
+                "#765bd6",
+                "#68db9e",
+                "#ffcbfc",
+                "#a54776",
+                "#286451"
+            ]
+        }]
+    },
+    options: {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: "Reasons Korean American Church Leaders Do Not Seek Therapy"
+            },
+            legend: {
+                position: "bottom"
             }
         }
     }
